1125effb6e06b462c0c757c436434f8b1466e9fb
Quincy Qiu <qiuqfang98@qq.com>
2023-03-18 15:42:02 +0800

    chore: 自动检测 react 版本

--- packages/react/index.js
+++ packages/react/index.js
@@ -4,6 +4,9 @@ module.exports = {
     "plugin:react-hooks/recommended", // react-hooks 推荐规则
     "plugin:react/jsx-runtime", // 解决react17后的jsx语法问题
   ],
+  settings: {
+    "version": "detect", // 自动选择已安装的版本
+  },
   rules: {
     "react/prop-types": 1,
   },
